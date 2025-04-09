db7d06a9fa5c49ea060dd3d6d0932d25ffde2b09
Farah Fairuz <farahfairuz014@gmail.com>
2025-04-09 15:01:35 +0700

    refactor: responsive breadcrumbs

--- src/components/common/Breadcrumbs.jsx
+++ src/components/common/Breadcrumbs.jsx
@@ -2,9 +2,9 @@ import { ChevronRight, Home } from "lucide-react";
 
 const Breadcrumbs = ({ paths }) => {
   return (
-    <nav className="flex gap-4 items-center px-16 py-10 bg-blue-50 text-base text-blue-600">
+    <nav className="flex flex-wrap gap-2 md:gap-4 items-center px-4 md:px-16 py-4 md:py-10 bg-blue-50 text-sm md:text-base text-blue-600 w-full">
       {paths.map((path, index) => (
-        <div key={index} className="flex gap-3 items-center">
+        <div key={index} className="flex gap-2 md:gap-3 items-center">
           {index === 0 && <Home size={16} className="text-blue-600" />}
           {index > 0 && <ChevronRight size={14} className="text-gray-800" />}
 
