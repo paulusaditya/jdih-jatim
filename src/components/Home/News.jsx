54c76eb137525e3c8d157013162fdf953530b112
paulusaditya <222410103052@unej.ac.id>
2025-04-16 01:21:57 +0700

    refactor: route berita -> news component home

--- src/components/Home/News.jsx
+++ src/components/Home/News.jsx
@@ -128,7 +128,7 @@ export default function NewsSection() {
             Berita Biro Hukum Sekretariat Daerah Jawa Timur
           </h2>
           <Link
-            to="/berita"
+            to="/news"
             className="flex items-center font-medium text-sm border rounded px-4 py-2 transition-colors text-blue-600 border-blue-600 hover:text-blue-800"
           >
             <span className="hidden md:inline">LIHAT SEMUA</span>
@@ -148,7 +148,7 @@ export default function NewsSection() {
                   <h3 className="text-xl md:text-2xl font-bold text-gray-900 leading-tight">{news.title}</h3>
                   <p className="text-gray-600">{news.content}</p>
                   <Link
-                    to={`/berita/detail-berita/${news.slug}`}
+                    to={`/news/detail-berita/${news.slug}`}
                     className="text-green-600 border border-green-600 rounded-md px-4 py-2 inline-flex items-center text-sm font-medium hover:bg-green-50 transition-colors"
                   >
                     Baca Selengkapnya
