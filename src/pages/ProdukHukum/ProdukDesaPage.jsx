01c31d15144b85c6646a0fc353ec00204a8031af
Farah Fairuz <farahfairuz014@gmail.com>
2025-04-24 21:57:18 +0700

    feat: add typetosectionid mapping to produkdesapage and produkkotapage

--- src/pages/ProdukHukum/ProdukDesaPage.jsx
+++ src/pages/ProdukHukum/ProdukDesaPage.jsx
@@ -1,24 +1,28 @@
 import LawPage from "./LawPage";
 
-const ProdukDesaPage = () => (
-  <LawPage
-    apiUrl="https://jdih.pisdev.my.id/api/v2/topics"
-    title="Produk Hukum Desa"
-    breadcrumbPaths={[
-      { label: "Beranda", path: "/" },
-      { label: "Produk Hukum Desa", path: "/site-pages/produk-desa" },
-    ]}
-    sectionId="40"
-    years={["", "2025", "2024", "2023", "2022", "2021", "2020", "2019"]}
-    documentTypes={[
-      "",
-      "Peraturan Desa",
-      "Keputusan Desa",
-    ]}
-    includeStatus={true}
-    includeCategory={true}
-    detailPath="/peraturan/produk-hukum-desa"
-  />
-);
+const ProdukDesaPage = () => {
+  const typeToSectionId = {
+    "Peraturan Desa": 35,
+    "Keputusan Desa": 36,
+  };
+
+  return (
+    <LawPage
+      apiUrl="https://jdih.pisdev.my.id/api/v2/topics"
+      title="Produk Hukum Desa"
+      breadcrumbPaths={[
+        { label: "Beranda", path: "/" },
+        { label: "Produk Hukum Desa", path: "/site-pages/produk-desa" },
+      ]}
+      sectionId="40"
+      years={["", "2025", "2024", "2023", "2022", "2021", "2020", "2019"]}
+      documentTypes={["", "Peraturan Desa", "Keputusan Desa"]}
+      includeStatus={true}
+      includeCategory={true}
+      detailPath="/peraturan/produk-hukum-desa"
+      typeToSectionId={typeToSectionId}
+    />
+  );
+};
 
 export default ProdukDesaPage;
--- src/pages/ProdukHukum/ProdukKotaPage.jsx
+++ src/pages/ProdukHukum/ProdukKotaPage.jsx
@@ -1,27 +1,41 @@
 import LawPage from "./LawPage";
 
-const ProdukKotaPage = () => (
-  <LawPage
-    apiUrl="https://jdih.pisdev.my.id/api/v2/topics"
-    title="Produk Hukum Kota/Kabupaten"
-    breadcrumbPaths={[
-      { label: "Beranda", path: "/" },
-      { label: "Produk Hukum Kota/Kabupaten", path: "/site-pages/produk-kota" },
-    ]}
-    sectionId="39"
-    years={["", "2025", "2024", "2023", "2022", "2021", "2020", "2019"]}
-    documentTypes={[
-      "",
-      "Peraturan Daerah",
-      "Peraturan Walikota",
-      "Keputusan Walikota",
-      "Surat Keputusan Walikota",
-      "Instruksi Walikota",
-    ]}
-    includeStatus={true}
-    includeCategory={true}
-    detailPath="/peraturan/produk-hukum-kota"
-  />
-);
+const ProdukKotaPage = () => {
+  const typeToSectionId = {
+    "Peraturan Daerah": 31,
+    "Peraturan Walikota": 32,
+    "Keputusan Walikota": 33,
+    "Surat Keputusan Walikota": 34,
+    "Instruksi Walikota": 35,
+  };
+
+  return (
+    <LawPage
+      apiUrl="https://jdih.pisdev.my.id/api/v2/topics"
+      title="Produk Hukum Kota/Kabupaten"
+      breadcrumbPaths={[
+        { label: "Beranda", path: "/" },
+        {
+          label: "Produk Hukum Kota/Kabupaten",
+          path: "/site-pages/produk-kota",
+        },
+      ]}
+      sectionId="39"
+      years={["", "2025", "2024", "2023", "2022", "2021", "2020", "2019"]}
+      documentTypes={[
+        "",
+        "Peraturan Daerah",
+        "Peraturan Walikota",
+        "Keputusan Walikota",
+        "Surat Keputusan Walikota",
+        "Instruksi Walikota",
+      ]}
+      includeStatus={true}
+      includeCategory={true}
+      detailPath="/peraturan/produk-hukum-kota"
+      typeToSectionId={typeToSectionId}
+    />
+  );
+};
 
 export default ProdukKotaPage;
