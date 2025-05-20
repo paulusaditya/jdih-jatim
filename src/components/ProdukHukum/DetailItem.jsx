c7458939fd14578daee45ff8672d99eb3d7521cd
paulusaditya <222410103052@mail.unej.ac.id>
2025-05-20 11:19:51 +0700

    style: change to 175px in detailitem

--- src/components/ProdukHukum/DetailItem.jsx
+++ src/components/ProdukHukum/DetailItem.jsx
@@ -3,7 +3,7 @@ import * as React from "react";
 function DetailItem({ label, value }) {
   return (
     <div className="flex px-0 py-3 border-b border-solid border-b-zinc-100 items-start">
-      <div className="text-base font-medium text-zinc-600 w-[250px] shrink-0">
+      <div className="text-base font-medium text-zinc-600 w-[175px] shrink-0">
         {label}
       </div>
       <div className="text-base text-zinc-600 break-words whitespace-pre-wrap flex-1 overflow-hidden text-ellipsis">
