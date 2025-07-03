03957c1173f5b1a633078f536862713844533027
paulusaditya <222410103052@mail.unej.ac.id>
2025-07-03 21:08:24 +0700

    refactor: update text alignment and overflow handling in DasarHukumPage component

--- src/components/Profil/DasarHukumPage.jsx
+++ src/components/Profil/DasarHukumPage.jsx
@@ -29,7 +29,7 @@ const DasarHukumPage = () => {
     <div className="py-8 px-4 md:px-6">
       <div className="max-w-7xl mx-auto bg-gray-100 p-6 md:p-8 rounded-xl shadow-md">
         <div
-          className="prose max-w-none prose-p:my-2 prose-li:my-1 prose-a:text-blue-600 prose-strong:text-green-700 prose-p:text-center prose-p:first:text-green-700 prose-p:nth-of-type(2):text-green-700 prose-p:nth-of-type(3):text-green-700"
+          className="prose max-w-none break-words overflow-x-auto prose-p:my-2 prose-li:my-1 prose-a:text-blue-600 prose-strong:text-green-700 prose-p:text-justify"
           dangerouslySetInnerHTML={{ __html: htmlContent }}
         />
       </div>
