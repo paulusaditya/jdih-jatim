63d01c16736c23a7d1171672bacf65eba42af275
paulusaditya <222410103052@mail.unej.ac.id>
2025-07-22 14:24:43 +0700

    refactor: enhance Footer component with updated contact information and improved layout

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,3 +1,4 @@
+// Footer.jsx
 "use client";
 
 import { useEffect, useState } from "react";
@@ -77,12 +78,70 @@ export default function Footer() {
 
             <div>
               <h3 className="text-lg font-semibold mb-4">
-                Kantor Biro Hukum Jawa Timur
+                Kantor Biro Hukum Sekretariat Daerah Provinsi Jawa Timur.
               </h3>
               <p className="text-sm mb-4">
-                Jl. Pahlawan No.110, Alun-alun Contong, Bubutan, Surabaya 60174,
-                Jawa Timur – Indonesia.
+                Lantai 4 Gedung Sekretariat Daerah Provinsi Jawa Timur.
+                <br />
+                Jl.Pahlawan No.110, Alun-alun Contong, Bubutan, Surabaya 60174,
+                Jawa Timur
               </p>
+              {/* Sitemap moved here */}
+              <h3 className="text-lg font-semibold mb-4 mt-10">Sitemap</h3>
+              <ul className="space-y-2">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/site-pages/about">Profil</Link>
+                </li>
+                <li>
+                  <Link to="/peraturan">Produk Hukum</Link>
+                </li>
+                <li>
+                  <Link to="/site-pages/staatsblad">Dokumen Hukum Lainnya</Link>
+                </li>
+                <li>
+                  <Link to="/news">Berita</Link>
+                </li>
+                <li>
+                  <Link to="https://sukma.jatimprov.go.id/fe/survey">
+                    Survey
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <h3 className="text-lg font-semibold mb-4">
+                Perpustakaan Hukum Biro Hukum Sekretariat Daerah Provinsi Jawa
+                Timur
+              </h3>
+              <p className="text-sm mb-4">
+                Lantai 1 Gedung Kantor Gubernur Jawa Timur
+                <br />
+                Jl. Pahlawan No.110, Alun-alun Contong, Bubutan, Surabaya 60174,
+                Jawa Timur.
+              </p>
+              {/* Analytics moved here */}
+              <h3 className="text-lg font-semibold mb-4 mt-8">Analytics</h3>
+              <ul className="space-y-2">
+                <li className="text-sm">
+                  Pengunjung Kemarin : {visitorStats.yesterday}
+                </li>
+                <li className="text-sm">
+                  Pengunjung Hari Ini : {visitorStats.today}
+                </li>
+                <li className="text-sm">
+                  Pengunjung Bulan Ini : {visitorStats.this_month}
+                </li>
+                <li className="text-sm">
+                  Pengunjung Tahun Ini : {visitorStats.this_year}
+                </li>
+                <li className="text-sm">
+                  Total Pengunjung : {visitorStats.total}
+                </li>
+              </ul>
             </div>
 
             <div>
@@ -90,11 +149,11 @@ export default function Footer() {
               <p className="text-sm mb-2">
                 031-3520881 031-3524001 (Psw. 1118)
               </p>
-              <p className="text-sm">support@jdih.jatimprov.go.id</p>
-            </div>
+              <p className="text-sm mb-4">support@jdih.jatimprov.go.id</p>
 
-            <div>
-              <h3 className="text-lg font-semibold mb-4">Media Sosial</h3>
+              {/* Media Sosial moved here */}
+              <h3 className="text-lg font-semibold mb-4 mt-25">Media Sosial</h3>
+
               <div className="flex space-x-4 gap-4">
                 <a
                   href="https://www.facebook.com/JDIH-Jatimprov-607856333021401/"
@@ -142,7 +201,7 @@ export default function Footer() {
                     fill="currentColor"
                     className="h-6 w-6"
                   >
-                    <path d="M12.525.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 1.4-.54 2.79-1.35 3.94-1.31 1.92-3.58 3.17-5.91 3.21-1.43.08-2.86-.31-4.08-1.03-2.02-1.19-3.44-3.37-3.65-5.71-.02-.5-.03-1-.01-1.49.18-1.9 1.12-3.72 2.58-4.96 1.66-1.44 3.98-2.13 6.15-1.72.02 1.48-.04 2.96-.04 4.44-.99-.32-2.15-.23-3.02.37-.63.41-1.11 1.04-1.36 1.75-.21.51-.15 1.07-.14 1.61.24 1.64 1.82 3.02 3.5 2.87 1.12-.01 2.19-.66 2.77-1.61.19-.33.4-.67.41-1.06.1-1.79.06-3.57.07-5.36.01-4.03-.01-8.05.02-12.07z"/>
+                    <path d="M12.525.02c1.31-.02 2.61-.01 3.91-.02.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 1.4-.54 2.79-1.35 3.94-1.31 1.92-3.58 3.17-5.91 3.21-1.43.08-2.86-.31-4.08-1.03-2.02-1.19-3.44-3.37-3.65-5.71-.02-.5-.03-1-.01-1.49.18-1.9 1.12-3.72 2.58-4.96 1.66-1.44 3.98-2.13 6.15-1.72.02 1.48-.04 2.96-.04 4.44-.99-.32-2.15-.23-3.02.37-.63.41-1.11 1.04-1.36 1.75-.21.51-.15 1.07-.14 1.61.24 1.64 1.82 3.02 3.5 2.87 1.12-.01 2.19-.66 2.77-1.61.19-.33.4-.67.41-1.06.1-1.79.06-3.57.07-5.36.01-4.03-.01-8.05.02-12.07z" />
                   </svg>
                 </a>
                 <a
@@ -157,56 +216,6 @@ export default function Footer() {
             </div>
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-12">
-            <div></div>
-
-            <div>
-              <h3 className="text-lg font-semibold mb-4">Sitemap</h3>
-              <ul className="space-y-2">
-                <li>
-                  <Link to="/">Home</Link>
-                </li>
-                <li>
-                  <Link to="/site-pages/about">Profil</Link>
-                </li>
-                <li>
-                  <Link to="/peraturan">Produk Hukum</Link>
-                </li>
-                <li>
-                  <Link to="/site-pages/staatsblad">Dokumen Hukum Lainnya</Link>
-                </li>
-                <li>
-                  <Link to="/news">Berita</Link>
-                </li>
-                <li>
-                  <Link to="https://sukma.jatimprov.go.id/fe/survey">
-                    Survey
-                  </Link>
-                </li>
-              </ul>
-            </div>
-
-            <div>
-              <h3 className="text-lg font-semibold mb-4">Analytics</h3>
-              <ul className="space-y-2">
-                <li className="text-sm">
-                  Pengunjung Kemarin : {visitorStats.yesterday}
-                </li>
-                <li className="text-sm">
-                  Pengunjung Hari Ini : {visitorStats.today}
-                </li>
-                <li className="text-sm">
-                  Pengunjung Bulan Ini : {visitorStats.this_month}
-                </li>
-                <li className="text-sm">
-                  Pengunjung Tahun Ini : {visitorStats.this_year}
-                </li>
-                <li className="text-sm">
-                  Total Pengunjung : {visitorStats.total}
-                </li>
-              </ul>
-            </div>
-          </div>
           {/* Copyright */}
           <div className="border-t border-green-900 pt-6">
             <div className="flex flex-col md:flex-row justify-between items-center">
